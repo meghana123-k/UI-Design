3effc83d4906b68d5cbc2e55128a4a3217fd2cff
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-03-18 20:59:52 +0530

    Create switchdemo.dart

---
+++ Lab-4/switchpackage/switchdemo/bin/switchdemo.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:switchdemo/switchdemo.dart';
+
+void main(List<String> arguments) {
+  switchdemo sd = switchdemo();
+  print("Enter Fruit: ");
+  String fruit = stdin.readLineSync()!;
+  sd.sdemo(fruit);
+}
