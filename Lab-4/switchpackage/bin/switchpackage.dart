d93bd9ae701f3e0e0781474a96ef942a91a1e8e9
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-03-19 21:42:09 +0530

    Update switchpackage.dart

--- Lab-4/switchpackage/bin/switchpackage.dart
+++ Lab-4/switchpackage/bin/switchpackage.dart
@@ -1,5 +1,6 @@
 import 'package:switchpackage/switchpackage.dart';
 import 'package:switchpackage/switchvar.dart';
+import 'package:switchpackage/switchlabel.dart';
 import 'dart:io';
 
 void main(List<String> arguments) {
@@ -12,4 +13,9 @@ void main(List<String> arguments) {
 	String season = stdin.readLineSync()!;
 	switchvar sv = switchvar();
 	sv.checkMonths(season);
+
+	switchlabel sl = switchlabel();
+	print("Enter Fruit");
+	String fruit = stdin.readLineSync()!;
+	sl.isfruitAvailable(fruit);
 }
